8eb56073825b8b8c1bc84d17c40728a7f184aaf3
Pieter Hintjens <ph@imatix.com>
2016-01-14 19:00:16 +0100

    Problem: port and verbose are hard-coded
    Solution: allow configuration via command line
    
    -p / --port portname
    -i / --interface wifiinterface
    -v / --verbose

--- src/midicast.c
+++ src/midicast.c
@@ -12,55 +12,79 @@
     =========================================================================
 */
 
-//  TODO: set interface via command line
-//  TODO: verbose on/off via command line
-
 #include <zyre.h>
 #include <alsa/asoundlib.h>
 
 int main (int argc, char *argv [])
 {
     int argn = 1;
-    char *port_name = "hw:1,0,0";
-    if (argn < argc)
-        port_name = argv [argn++];
-
+    if (argn == argc
+    ||  streq (argv [argn], "-h") || streq (argv [argn], "--help")) {
+        puts ("midicast [-v] [-p port] [-i interface]");
+        puts ("Reads MIDI events from port and sends to Zyre MIDI group");
+        puts (" -h, --help: this help");
+        puts (" -v, --verbose: trace events as they happen");
+        puts (" -p, --port: specify port name, e.g. '-p hw:1,0,0'");
+        puts (" -i, --interface: specify WiFi interface, e.g. '-i wlan0'");
+        return 0;
+    }
+    char *midi_port_name = "hw:1,0,0";
+    char *wifi_interface = NULL;
+    bool verbose = false;
+    while (argn < argc) {
+        if (streq (argv [argn], "-p") || streq (argv [argn], "--port"))
+            midi_port_name = argv [++argn];
+        else
+        if (streq (argv [argn], "-i") || streq (argv [argn], "--interface"))
+            wifi_interface = argv [++argn];
+        else
+        if (streq (argv [argn], "-v") || streq (argv [argn], "--verbose"))
+            verbose = true;
+        argn++;
+    }
     snd_rawmidi_t *input;
-    int rc = snd_rawmidi_open (&input, NULL, port_name, 0);
+    int rc = snd_rawmidi_open (&input, NULL, midi_port_name, 0);
     if (rc < 0) {
-        zsys_error ("cannot open port \"%s\": %s", port_name, snd_strerror (rc));
+        zsys_error ("cannot open port \"%s\": %s", midi_port_name, snd_strerror (rc));
         return 0;
     }
     //  Flush any waiting input
     zclock_sleep (100);
-    zsys_info ("casting MIDI events from %s", port_name);
+    zsys_info ("casting MIDI events from %s", midi_port_name);
 
     zyre_t *zyre = zyre_new (NULL);
+    if (wifi_interface)
+        zyre_set_interface (zyre, wifi_interface);
     zyre_start (zyre);
     zyre_join (zyre, "MIDI");
     zsys_info ("this player is %s", zyre_name (zyre));
 
     while (!zsys_interrupted) {
         byte buffer [256];
-        int bytes_read = snd_rawmidi_read (input, buffer, sizeof (buffer));
-        if (bytes_read == -EAGAIN)
+        int rc = snd_rawmidi_read (input, buffer, sizeof (buffer));
+        if (rc == -EAGAIN)
             continue;
-
-        if (bytes_read < 0) {
-            zsys_error ("cannot read from port \"%s\": %s", port_name, snd_strerror (bytes_read));
+        else
+        if (rc == -EINTR) {
+            printf (" interrupted\n");
+            break;
+        }
+        if (rc < 0) {
+            zsys_error ("cannot read MIDI data: %s", snd_strerror (rc));
             break;
         }
         //  Broadcast the MIDI event
         zmsg_t *msg = zmsg_new ();
-        zmsg_addmem (msg, buffer, bytes_read);
+        zmsg_addmem (msg, buffer, rc);
         zyre_shout (zyre, "MIDI", &msg);
 
-        //  Dump the buffer for debugging purposes
-        printf ("%d:", bytes_read);
-        int byte_nbr;
-        for (byte_nbr = 0; byte_nbr < bytes_read; byte_nbr++)
-            printf (" %02X", buffer [byte_nbr]);
-        printf ("\n");
+        if (verbose) {
+            printf ("%d:", rc);
+            int byte_nbr;
+            for (byte_nbr = 0; byte_nbr < rc; byte_nbr++)
+                printf (" %02X", buffer [byte_nbr]);
+            printf ("\n");
+        }
     }
     snd_rawmidi_close (input);
     zyre_destroy (&zyre);
--- src/midigrab.c
+++ src/midigrab.c
@@ -12,34 +12,58 @@
     =========================================================================
 */
 
-//  TODO: set interface via command line
-//  TODO: verbose on/off via command line
-
 #include <zyre.h>
 #include <alsa/asoundlib.h>
 
 int main (int argc, char *argv [])
 {
     int argn = 1;
-    char *port_name = "hw:2,0";
-    if (argn < argc)
-        port_name = argv [argn++];
-
-    snd_rawmidi_t *output;
-    int rc = snd_rawmidi_open (NULL, &output, port_name, SND_RAWMIDI_SYNC);
-    if (rc < 0) {
-        zsys_error ("cannot open port \"%s\": %s", port_name, snd_strerror (rc));
+    if (argn == argc
+    ||  streq (argv [argn], "-h") || streq (argv [argn], "--help")) {
+        puts ("midicast [-v] [-p port] [-i interface]");
+        puts ("Reads MIDI events from port and sends to Zyre MIDI group");
+        puts (" -h, --help: this help");
+        puts (" -v, --verbose: trace events as they happen");
+        puts (" -p, --port: specify port name, e.g. '-p hw:1,0,0'");
+        puts (" -i, --interface: specify WiFi interface, e.g. '-i wlan0'");
         return 0;
     }
-    zsys_info ("forwarding MIDI cast to %s", port_name);
+    char *midi_port_name = "hw:2,0";
+    char *wifi_interface = NULL;
+    bool verbose = false;
+    while (argn < argc) {
+        if (streq (argv [argn], "-p") || streq (argv [argn], "--port"))
+            midi_port_name = argv [++argn];
+        else
+        if (streq (argv [argn], "-i") || streq (argv [argn], "--interface"))
+            wifi_interface = argv [++argn];
+        else
+        if (streq (argv [argn], "-v") || streq (argv [argn], "--verbose"))
+            verbose = true;
+        argn++;
+    }
+
+    snd_rawmidi_t *output;
+    int rc = snd_rawmidi_open (NULL, &output, midi_port_name, SND_RAWMIDI_SYNC);
+    if (rc < 0) {
+        zsys_error ("cannot open port \"%s\": %s", midi_port_name, snd_strerror (rc));
+        return 0;
+    }
+    zsys_info ("forwarding MIDI cast to %s", midi_port_name);
 
     zyre_t *zyre = zyre_new (NULL);
+    if (wifi_interface)
+        zyre_set_interface (zyre, wifi_interface);
     zyre_start (zyre);
     zyre_join (zyre, "MIDI");
     zsys_info ("this player is %s", zyre_name (zyre));
 
     while (!zsys_interrupted) {
         zyre_event_t *event = zyre_event_new (zyre);
+        if (!event) {
+            printf (" interrupted\n");
+            break;
+        }
         if (zyre_event_type (event) == ZYRE_EVENT_JOIN)
             zsys_info ("[%s] player joined", zyre_event_name (event));
         else
@@ -52,14 +76,14 @@ int main (int argc, char *argv [])
                 //  Forward the MIDI event
                 snd_rawmidi_write (output, zframe_data (frame), zframe_size (frame));
 
-                //  Dump the buffer for debugging purposes
-                printf ("%zd:", zframe_size (frame));
-                int byte_nbr;
-                for (byte_nbr = 0; byte_nbr < zframe_size (frame); byte_nbr++)
-                    printf (" %02X", zframe_data (frame) [byte_nbr]);
-                printf ("\n");
+                if (verbose) {
+                    printf ("%zd:", zframe_size (frame));
+                    int byte_nbr;
+                    for (byte_nbr = 0; byte_nbr < zframe_size (frame); byte_nbr++)
+                        printf (" %02X", zframe_data (frame) [byte_nbr]);
+                    printf ("\n");
+                }
             }
-            break;
         }
         zyre_event_destroy (&event);
     }
